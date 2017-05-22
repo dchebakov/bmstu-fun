297f839ff4040741d1a790c18f6c24d647abe073
Dmitry <chad-d@mail.ru>
2017-05-23 01:09:58 +0300

    add mathanalysis

---
+++ src/BOLT/tasks/mathanalysis.py
@@ -0,0 +1,33 @@
+import math
+from django.shortcuts import render
+from ..models import Task, Section
+from .. import views
+from ..models import Task, Section, Comment, Thanks, UserProfile
+from ..forms import CommentForm
+
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+import re
+
+def comments(request, task):
+    is_like = True
+    if request.user.is_authenticated():
+        is_like = Thanks.objects.filter(task=task,
+                                        user=UserProfile.objects.get(user=request.user)).exists()
+
+    return dict({'comments': Comment.objects.filter(task=task),
+                 'comment_form': CommentForm(), 'is_like': is_like},
+                **views.get_default_data(request))
+
+
+def task_decorate(function):
+    def wrapper(request):
+        task = Task.objects.get(function_name=function.__name__)
+        solve = function(request)
+        return render(request, 'task.html',
+                      dict({'template': 'solutions/mathanalysis/' +
+                                            function.__name__ + '.html',
+                            'task': task, 'solve': solve},
+                           **comments(request, task)))
+
+
