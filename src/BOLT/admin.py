bdaa122c89d40063b526b84f79e47795b438042e
Dmitry <chad-d@mail.ru>
2017-05-22 20:34:39 +0300

    added the ability for administrators to edit comments

--- src/BOLT/admin.py
+++ src/BOLT/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Task, News, NewTask
+from .models import Task, News, NewTask, Comment
 
 admin.site.register(Task)
 admin.site.register(News)
 admin.site.register(NewTask)
+admin.site.register(Comment)
