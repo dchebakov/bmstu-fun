bb1b5518a4aba824f09f942072126b703fd2fce6
sashaalesin <sashaalesin@yandex.ru>
2017-10-21 18:37:31 +0300

    Revert "add tasks for technopark"
    This reverts commit e99a52f2cc87d75aa6cb34639b5e813087c36cd2.

--- BOLT_PROJECT/bolt_miniproject/src/bolt_sandbox/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class ChangeTitleForm(forms.Form):
-    title = forms.CharField(label='Условие',
-                            required=True,
-                            widget=forms.Textarea(
-        attrs={'rows':7, 'class':'form-control', 'placeholder':'Введите условие задачи'}
-    ))
