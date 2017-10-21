bf54a766c5c8d81d66a45b395593da87c8f26519
sashaalesin <sashaalesin@yandex.ru>
2017-10-22 01:36:07 +0300

    add descriptions for tasks

--- src/BOLT/models.py
+++ src/BOLT/models.py
@@ -94,6 +94,7 @@ class TaskManager(models.Manager):
 
 class Task(models.Model):
     title = models.TextField()
+    description = models.TextField(default='')
     section = models.ForeignKey(Section)
     rating = models.IntegerField(default=0)
     date_created = models.DateTimeField(auto_now_add=True)
